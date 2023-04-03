ee993aaadfbdae8eab8d9fd6ae9e1114781ac978
Viacheslav Privalov <privalov2306@yandex.ru>
2023-04-03 15:58:19 +0300

    Домашка сделана. Только я не использовал интерфейс, а обошелся одним абстрактным классом. И получается, что класс ServiceStation вообще больше не нужен.

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+public class Car extends Vehicle {
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + this.getModelName());
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            this.updateTyre();
+        }
+        this.checkEngine();
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,46 @@
+public abstract class Vehicle {
+    private String modelName;
+    private int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+
+        System.out.println("Проверяем прицеп");
+    }
+
+    public void check() {
+        System.out.println("Обслуживаем " + this.getModelName());
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+            this.updateTyre();
+        }
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+}
